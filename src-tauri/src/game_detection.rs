054e509e186e50eaaf55933b7f03b95b235a46d8
AnthonyFuller <anthony@hitmods.com>
2024-08-25 04:43:55 +0100

    feat(linux/game detection): support legendary on linux, move into one function

--- src-tauri/src/game_detection.rs
+++ src-tauri/src/game_detection.rs
@@ -3,11 +3,7 @@ mod windows {
 	use registry::{Data, Hive, Security};
 	use std::os::windows::process::CommandExt;
 	use anyhow::anyhow;
-	use std::{
-		path::Path,
-		process::Command
-	};
-	use serde_json::Value;
+	use std::{process::Command, path::Path};
 }
 
 use std::{
@@ -21,6 +17,7 @@ use hashbrown::HashMap;
 use hitman_commons::game::GameVersion;
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use specta::Type;
 use tryvial::try_fn;
 
@@ -38,10 +35,13 @@ pub struct GameInstall {
 	pub path: PathBuf
 }
 
-#[cfg(target_os = "windows")]
 #[try_fn]
 #[context("Couldn't detect installed games")]
 pub fn detect_installs() -> Result<Vec<GameInstall>> {
+	#[cfg(target_os = "linux")]
+	let home = home::home_dir().expect("Unable to find home directory");
+
+	#[cfg(target_os = "windows")]
 	let legendary_installed_paths = [
 		Path::new(&std::env::var("USERPROFILE").context("%USERPROFILE%")?)
 			.join(".config")
@@ -50,6 +50,14 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 		Path::new(&std::env::var("APPDATA").context("%APPDATA%")?)
 			.join("heroic")
 			.join("legendaryConfig")
+			.join("legendary")
+			.join("installed.json")
+	];
+
+	#[cfg(target_os = "linux")]
+	let legendary_installed_paths = [
+		home
+			.join(".config")
 			.join("legendary")
 			.join("installed.json")
 	];
@@ -91,91 +99,22 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 		}
 	}
 
-	// EGL installs
-	if let Ok(hive) = Hive::CurrentUser.open(r#"Software\Epic Games\EOS"#, Security::Read) {
-		match hive.value("ModSdkMetadataDir") {
-			Ok(Data::String(d)) => {
-				if let Ok(entries) = fs::read_dir(d.to_string_lossy()) {
-					for entry in entries
-						.filter_map(|x| x.ok())
-						.filter(|x| x.file_type().ok().map(|x| x.is_file()).unwrap_or(false))
-					{
-						if let Ok(manifest_data) = serde_json::from_slice::<Value>(
-							&fs::read(entry.path())
-								.with_context(|| format!("Reading EOS manifest {}", entry.path().display()))?
-						) {
-							// H3
-							if manifest_data
-								.get("AppName")
-								.context("AppName")?
-								.as_str()
-								.context("as_str")? == "Eider"
-							{
-								check_paths.push((
-									PathBuf::from(
-										manifest_data
-											.get("InstallLocation")
-											.context("InstallLocation")?
-											.as_str()
-											.context("as_str")?
-									),
-									"Epic Games"
-								));
-							}
-
-							// H1
-							if manifest_data
-								.get("AppName")
-								.context("AppName")?
-								.as_str()
-								.context("as_str")? == "Barbet"
-							{
-								check_paths.push((
-									PathBuf::from(
-										manifest_data
-											.get("InstallLocation")
-											.context("InstallLocation")?
-											.as_str()
-											.context("as_str")?
-									),
-									"Epic Games"
-								));
-							}
-						}
-					}
-				}
-			}
-
-			Ok(_) => Err(anyhow::anyhow!(
-				"Registry key ModSdkMetadataDir was not string".to_owned()
-			))?,
-
-			Err(_) => {}
-		}
-	}
-
-	// 	Steam installs
-	if let Ok(hive) = Hive::CurrentUser.open(r#"Software\Valve\Steam"#, Security::Read) {
-		match hive.value("SteamPath") {
-			Ok(Data::String(d)) => {
+	#[cfg(target_os = "linux")]
+	// Steam installs (linux)
+	{
+		let steampath = home.join(".steam/debian-installation");
+		match steampath.exists() {
+			true => {
 				if let Ok(s) = fs::read_to_string(
-					if Path::new(&d.to_string_lossy())
-						.join("config")
-						.join("libraryfolders.vdf")
-						.exists()
-					{
-						Path::new(&d.to_string_lossy())
-							.join("config")
-							.join("libraryfolders.vdf")
+					if steampath.join("config").join("libraryfolders.vdf").exists() {
+						steampath.join("config").join("libraryfolders.vdf")
 					} else {
-						Path::new(&d.to_string_lossy())
-							.join("steamapps")
-							.join("libraryfolders.vdf")
+						steampath.join("steamapps").join("libraryfolders.vdf")
 					}
 				) {
 					let folders: HashMap<String, SteamLibraryFolder> =
 						keyvalues_serde::from_str(&s).context("VDF parse")?;
-
+	
 					for folder in folders.values() {
 						// H1, H1 free trial
 						if folder.apps.contains_key("236870") || folder.apps.contains_key("649780") {
@@ -187,7 +126,7 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 								"Steam"
 							));
 						}
-
+	
 						// H2
 						if folder.apps.contains_key("863550") {
 							check_paths.push((
@@ -198,7 +137,7 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 								"Steam"
 							));
 						}
-
+	
 						// H3, H3 demo
 						if folder.apps.contains_key("1659040") || folder.apps.contains_key("1847520") {
 							check_paths.push((
@@ -212,97 +151,98 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 					}
 				};
 			}
-
-			Ok(_) => {
-				bail!("Registry key SteamPath was not string");
-			}
-
-			Err(_) => {}
+	
+			false => {}
 		}
 	}
 
-	// Microsoft install of H3
-	if let Ok(proc_out) = Command::new("powershell")
-		.args(["-Command", "Get-AppxPackage -Name IOInteractiveAS.PC-HITMAN3-BaseGame"])
-		.creation_flags(0x08000000) // CREATE_NO_WINDOW
-		.output()
+	#[cfg(target_os = "windows")]
 	{
-		if let Some(line) = String::from_utf8_lossy(&proc_out.stdout)
-			.lines()
-			.find(|x| x.starts_with("InstallLocation"))
-		{
-			check_paths.push((
-				fs::read_link(line.split(':').skip(1).collect::<Vec<_>>().join(":").trim())?,
-				"Microsoft"
-			));
-		}
-	}
+		// EGL installs
+		if let Ok(hive) = Hive::CurrentUser.open(r#"Software\Epic Games\EOS"#, Security::Read) {
+			match hive.value("ModSdkMetadataDir") {
+				Ok(Data::String(d)) => {
+					if let Ok(entries) = fs::read_dir(d.to_string_lossy()) {
+						for entry in entries
+							.filter_map(|x| x.ok())
+							.filter(|x| x.file_type().ok().map(|x| x.is_file()).unwrap_or(false))
+						{
+							if let Ok(manifest_data) = serde_json::from_slice::<Value>(
+								&fs::read(entry.path())
+									.with_context(|| format!("Reading EOS manifest {}", entry.path().display()))?
+							) {
+								// H3
+								if manifest_data
+									.get("AppName")
+									.context("AppName")?
+									.as_str()
+									.context("as_str")? == "Eider"
+								{
+									check_paths.push((
+										PathBuf::from(
+											manifest_data
+												.get("InstallLocation")
+												.context("InstallLocation")?
+												.as_str()
+												.context("as_str")?
+										),
+										"Epic Games"
+									));
+								}
 
-	// GOG install of H1
-	if let Ok(hive) = Hive::LocalMachine.open(r#"Software\WOW6432Node\GOG.com\Games\1545448592"#, Security::Read) {
-		match hive.value("path") {
-			Ok(Data::String(d)) => {
-				check_paths.push((PathBuf::from(&d.to_string_lossy()), "GOG"));
-			}
-
-			_ => {
-				bail!("GOG install path was not string");
-			}
-		}
-	}
-
-	let mut game_installs = vec![];
-
-	for (path, platform) in check_paths {
-		// Game folder has Retail
-		let subfolder_retail = path.join("Retail").is_dir();
-
-		if subfolder_retail {
-			game_installs.push(GameInstall {
-				path: path.join("Retail"),
-				platform: platform.into(),
-				version: if path.join("Retail").join("HITMAN3.exe").is_file() {
-					GameVersion::H3
-				} else if path.join("Retail").join("HITMAN2.exe").is_file() {
-					GameVersion::H2
-				} else if path.join("Retail").join("HITMAN.exe").is_file() {
-					GameVersion::H1
-				} else {
-					bail!("Unknown game added to check paths");
+								// H1
+								if manifest_data
+									.get("AppName")
+									.context("AppName")?
+									.as_str()
+									.context("as_str")? == "Barbet"
+								{
+									check_paths.push((
+										PathBuf::from(
+											manifest_data
+												.get("InstallLocation")
+												.context("InstallLocation")?
+												.as_str()
+												.context("as_str")?
+										),
+										"Epic Games"
+									));
+								}
+							}
+						}
+					}
 				}
-			});
+
+				Ok(_) => Err(anyhow::anyhow!(
+					"Registry key ModSdkMetadataDir was not string".to_owned()
+				))?,
+
+				Err(_) => {}
+			}
 		}
-	}
 
-	game_installs
-		.into_iter()
-		.unique_by(|x| x.path.to_owned())
-		.sorted_unstable_by_key(|x| x.version)
-		.collect()
-}
-
-#[cfg(target_os = "linux")]
-#[try_fn]
-#[context("Couldn't detect installed games")]
-pub fn detect_installs() -> Result<Vec<GameInstall>> {
-	let mut check_paths = vec![];
-
-	// 	Steam installs
-	match home::home_dir() {
-		Some(home) => {
-			let steampath = home.join(".steam/debian-installation");
-			match steampath.exists() {
-				true => {
+		// 	Steam installs
+		if let Ok(hive) = Hive::CurrentUser.open(r#"Software\Valve\Steam"#, Security::Read) {
+			match hive.value("SteamPath") {
+				Ok(Data::String(d)) => {
 					if let Ok(s) = fs::read_to_string(
-						if steampath.join("config").join("libraryfolders.vdf").exists() {
-							steampath.join("config").join("libraryfolders.vdf")
+						if Path::new(&d.to_string_lossy())
+							.join("config")
+							.join("libraryfolders.vdf")
+							.exists()
+						{
+							Path::new(&d.to_string_lossy())
+								.join("config")
+								.join("libraryfolders.vdf")
 						} else {
-							steampath.join("steamapps").join("libraryfolders.vdf")
+							Path::new(&d.to_string_lossy())
+								.join("steamapps")
+								.join("libraryfolders.vdf")
 						}
 					) {
 						let folders: HashMap<String, SteamLibraryFolder> =
 							keyvalues_serde::from_str(&s).context("VDF parse")?;
-		
+
 						for folder in folders.values() {
 							// H1, H1 free trial
 							if folder.apps.contains_key("236870") || folder.apps.contains_key("649780") {
@@ -314,7 +254,7 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 									"Steam"
 								));
 							}
-		
+
 							// H2
 							if folder.apps.contains_key("863550") {
 								check_paths.push((
@@ -325,7 +265,7 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 									"Steam"
 								));
 							}
-		
+
 							// H3, H3 demo
 							if folder.apps.contains_key("1659040") || folder.apps.contains_key("1847520") {
 								check_paths.push((
@@ -339,13 +279,45 @@ pub fn detect_installs() -> Result<Vec<GameInstall>> {
 						}
 					};
 				}
-		
-				false => {}
+
+				Ok(_) => {
+					bail!("Registry key SteamPath was not string");
+				}
+
+				Err(_) => {}
 			}
-		},
-		None => {},
+		}
+
+		// Microsoft install of H3
+		if let Ok(proc_out) = Command::new("powershell")
+			.args(["-Command", "Get-AppxPackage -Name IOInteractiveAS.PC-HITMAN3-BaseGame"])
+			.creation_flags(0x08000000) // CREATE_NO_WINDOW
+			.output()
+		{
+			if let Some(line) = String::from_utf8_lossy(&proc_out.stdout)
+				.lines()
+				.find(|x| x.starts_with("InstallLocation"))
+			{
+				check_paths.push((
+					fs::read_link(line.split(':').skip(1).collect::<Vec<_>>().join(":").trim())?,
+					"Microsoft"
+				));
+			}
+		}
+
+		// GOG install of H1
+		if let Ok(hive) = Hive::LocalMachine.open(r#"Software\WOW6432Node\GOG.com\Games\1545448592"#, Security::Read) {
+			match hive.value("path") {
+				Ok(Data::String(d)) => {
+					check_paths.push((PathBuf::from(&d.to_string_lossy()), "GOG"));
+				}
+
+				_ => {
+					bail!("GOG install path was not string");
+				}
+			}
+		}
 	}
-	
 
 	let mut game_installs = vec![];
 
