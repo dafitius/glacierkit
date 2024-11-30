b826f871220646a74862b8147845c9321c577cb4
dafitius <70489995+dafitius@users.noreply.github.com>
2024-11-30 17:40:20 +0100

    format and clippy ✨

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -40,7 +40,6 @@ use biome::format_json;
 use dashmap::DashMap;
 use editor_connection::EditorConnection;
 use entity::get_diff_info;
-use std::fs::OpenOptions;
 use event_handling::{
 	repository_patch::handle_repository_patch_event, resource_overview::handle_resource_overview_event,
 	tools::handle_tool_event, unlockables_patch::handle_unlockables_patch_event
@@ -66,11 +65,12 @@ use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
 use serde_json::{from_slice, json, to_value, to_vec, Value};
 use show_in_folder::show_in_folder;
+use std::fs::OpenOptions;
+use std::path::StripPrefixError;
 use tauri::{
 	api::{dialog::blocking::FileDialogBuilder, process::Command},
 	async_runtime, AppHandle, Manager
 };
-use std::path::StripPrefixError;
 use tauri_plugin_aptabase::{EventTracker, InitOptions};
 use tauri_plugin_log::LogTarget;
 use tryvial::try_fn;
@@ -354,8 +354,8 @@ fn event(app: AppHandle, event: Event) {
 										recent_projects: app_settings.load().recent_projects.clone()
 									}))
 								)?;
-							},
-							QuickStartEvent::RefreshRecentList {id} => {
+							}
+							QuickStartEvent::RefreshRecentList { id } => {
 								send_request(
 									&app,
 									Request::Editor(EditorRequest::QuickStart(QuickStartRequest::RefreshRecentList {
@@ -363,8 +363,15 @@ fn event(app: AppHandle, event: Event) {
 										recent_projects: app_settings.load().recent_projects.clone()
 									}))
 								)?;
-							},
-							QuickStartEvent::CreateLocalProject { id, project_id, name, author, path, version } => {
+							}
+							QuickStartEvent::CreateLocalProject {
+								id,
+								project_id,
+								name,
+								author,
+								path,
+								version
+							} => {
 								fn replace_prefix(
 									p: impl AsRef<Path>,
 									from: impl AsRef<Path>,
@@ -390,7 +397,7 @@ fn event(app: AppHandle, event: Event) {
 											if file.is_file() {
 												let file_path = replace_prefix(outpath, "smf-mod-main", &project_dir)
 													.context("Failed to move mod template files")?;
-												fs::create_dir_all(&file_path.parent().unwrap())
+												fs::create_dir_all(file_path.parent().unwrap())
 													.context("Failed to create necessary project folder")?;
 												let mut outfile = fs::File::create(&file_path).unwrap();
 												std::io::copy(&mut file, &mut outfile).unwrap();
@@ -874,13 +881,8 @@ fn event(app: AppHandle, event: Event) {
 
 							app_state.project.store(None);
 
-							send_request(
-								&app,
-								Request::Global(GlobalRequest::SetWindowTitle(
-									"".to_string()
-								))
-							)?;
-			
+							send_request(&app, Request::Global(GlobalRequest::SetWindowTitle("".to_string())))?;
+
 							app_state.fs_watcher.store(None);
 
 							finish_task(&app, task)?;
